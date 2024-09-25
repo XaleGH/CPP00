1bba30faade52d1ac7558e0cfe069ad70213ccfc
Axel Saux <asaux@c1r3p5.42perpignan.fr>
2024-09-25 16:27:06 +0200

    ex02 + start ex03

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asaux <asaux@student.42perpignan.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 15:58:16 by asaux             #+#    #+#             */
+/*   Updated: 2024/09/25 16:05:06 by asaux            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "Address of the string variable: " << &str << std::endl;
+	std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of the string variable: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+
+	return (0);
+}
